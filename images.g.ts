420e064ffc0b95eb9edf5182ae770d9f522d8575
Carlos Vargas <carlos.vargas.7e8@itb.cat>
2025-11-24 18:59:03 +0100

    prueba de asset

---
+++ images.g.ts
@@ -0,0 +1,45 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+b b b b b 2 2 e e b b b b b 
+b b b b b 2 f e e b b b b b 
+b b f f e e e e e f f f b b 
+b f f e e e 2 e f f f f f b 
+b f f e d d d d d e f f f b 
+b f d d d d d d d d d d f b 
+b f d f d d d d d d f d f b 
+e f d d d d f f d d d d f e 
+b e d d d d d d d d d d e b 
+b b e e e e e e e e e e b b 
+b b b b f d d d d f b b b b 
+b e e f f f e e f f f e e b 
+e d d e f 5 e e f f e d d e 
+e d d e f f e e f f e d d e 
+b b b b c c c c c c b b b b 
+b b b b c b b b b c b b b b 
+b b b b c b b b b c b b b b 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
